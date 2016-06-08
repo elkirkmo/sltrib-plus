8c3a1f19df4b6cc60744f05ec4e629ae7ecfb69b
Chris Kirkham <ckirkham@fandango.com>
2016-06-08 14:41:51 -0700

    added readme

---
+++ chrome/popup.js
@@ -0,0 +1,21 @@
+if (window.location.host === "www.reddit.com") {
+	document.getElementById("header-bottom-left").style = "background-image: none !important; height: 50px !important;";
+	document.querySelector(".pagename.redditname").style = "margin-top: 30px;";
+	document.querySelector('.side a[href="https://www.reddit.com"]').style = "display: none;";
+}
+
+if (window.location.host === "www.sltrib.com") {
+	var links = document.querySelectorAll("a.headline");
+	//console.log(links);
+	for (var i = 0; i < links.length; i++){
+		//console.log('hey');
+		links[i].href += "?fullpage=1";
+	}
+
+	// document.querySelector('#main-content .col-md-16').style = 'width: 100% !important';
+	 document.querySelector('.col-md-8.pull-right').style = 'display:none;';
+	// document.querySelector('.pull-left').style='display: none;';
+	// document.querySelector('.content-full-article.col-md-8').style = 'width: 100% !important;';
+	// document.querySelectorAll("p.TEXT_w_Indent").style = "font-size: 20px !important; line-height: 30px !important; font-family: serif !important;";
+	console.log(links);
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+<h1>SLTrib Enhancer</h1>
+
+<p>An extension to update and beautify the reading experience of SLTrib.com - a fine news organization</p>
