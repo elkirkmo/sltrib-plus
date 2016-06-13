618f19b258318819c49818386007553c98fe602b
Chris Kirkham <ckirkham@fandango.com>
2016-06-13 14:32:37 -0700

    Updated the fullpage link modifier. Added some styles. Added script to move editor's picks below top4.

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,8 +1,28 @@
 if (window.location.host === "www.sltrib.com") {
-	var links = document.querySelectorAll("a.headline");
-	//console.log(links);
-	for (var i = 0; i < links.length; i++){
-		//console.log('hey');
-		links[i].href += "?fullpage=1";
+	//fullpage by default
+	var links = document.querySelectorAll("a");
+	var news = "/news/";
+	console.log(links);
+	function addFullPage(links) {
+		for (var i = 0; i < links.length; i++){
+			console.log(links[i].baseURI);
+			
+			if (links[i].host === "www.sltrib.com" && (links[i].href.indexOf(news) > -1)) {
+				console.log(i);
+				links[i].href += "?fullpage=1";
+			}
+		}
 	}
+
+	addFullPage(links);
+	// addFullPage(imgLinks);
+
+	//append editor's picks after top 4 stories.
+	var editorsPicks = document.querySelector(".col-md-8.pull-right.content-area.content-articles-list");
+	var topFour = document.querySelector('.col-md-16.content-area.content-articles-grid-large.hidden-sm.hidden-xs');
+	editorsPicks.cloneNode(true);
+	editorsPicks.parentNode.removeChild(editorsPicks);
+	topFour.appendChild(editorsPicks);
+	editorsPicks.style = "display: block;";
+	
 }
