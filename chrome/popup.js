61654db17f20597667f714adc1765e1587ec356e
Chris Kirkham <ckirkham@fandango.com>
2017-04-20 14:38:31 -0600

    Major code rehaul, updated favicons.

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,22 +1,13 @@
 if (window.location.host === "www.sltrib.com") {
-	//fullpage by default
-	var links = document.querySelectorAll("a");
-	var news = "/news/";
-	console.log(links);
-	function addFullPage(links) {
-		for (var i = 0; i < links.length; i++){
-			console.log(links[i].baseURI);
-			
-			if (links[i].host === "www.sltrib.com" && (links[i].href.indexOf(news) > -1)) {
-				console.log(i);
-				links[i].href += "?fullpage=1";
-			}
-		}
-	}
-
-	addFullPage(links);
-	// addFullPage(imgLinks);
-
+    var links = document.querySelectorAll("a");
+    function addFullPage(links) {
+    links.forEach(function(a){
+        if (a.host === 'www.sltrib.com' && a.hash !== '#disqus_thread'){
+            a.href += '?fullpage=1'
+        }
+    });
+    }
+    addFullPage(links);
 	//append editor's picks after top 4 stories.
 	var editorsPicks = document.querySelector(".col-md-8.pull-right.content-area.content-articles-list");
 	var topFour = document.querySelector('.col-md-16.content-area.content-articles-grid-large.hidden-sm.hidden-xs');
