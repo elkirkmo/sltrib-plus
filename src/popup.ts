820c7a73a08270ca57354ffc7cb623de32881fc0
Chris Kirkham <ckirkham@fandango.com>
2017-04-24 20:55:59 -0600

    Removing es6 linting.

--- src/popup.ts
+++ src/popup.ts
@@ -1,22 +1,21 @@
 var window: Window;
 var document: Document;
-const links:NodeListOf<Element> = document.querySelectorAll('a');
-const editorsPicks = document.querySelector(".col-md-8.pull-right.content-area.content-articles-list");
-const topFour = document.querySelector('.col-md-16.content-area.content-articles-grid-large.hidden-sm.hidden-xs');
+let links = Array.prototype.slice.call(document.querySelectorAll('a'));
+let editorsPicks = <HTMLElement> document.querySelector('.col-md-8.pull-right.content-area.content-articles-list');
+let topFour = <HTMLElement> document.querySelector('.col-md-16.content-area.content-articles-grid-large.hidden-sm.hidden-xs');
 
-const addFullPage = (linkArray:NodeListOf<Element>) => {
-  if (window.location.host === "www.sltrib.com") {
-    linkArray.forEach(function(a){
-        if (a.host === 'www.sltrib.com' && a.hash !== '#disqus_thread'){
-            a.href += '?fullpage=1'
-        }
-    });
+const addFullPage: Function = (linkList) => {
+  if (window.location.host === 'www.sltrib.com') {
+    for (let link of linkList) {
+      if (link.host === 'www.sltrib.com' && link.hash !== '#disqus_thread') {
+        link.href += '?fullpage=1';
+      }
     }
+  }
+}
   addFullPage(links);
-
   // append editor's picks after top 4 stories.
   editorsPicks.cloneNode(true);
   editorsPicks.parentNode.removeChild(editorsPicks);
   topFour.appendChild(editorsPicks);
   editorsPicks.style = 'display: block;';
-}
